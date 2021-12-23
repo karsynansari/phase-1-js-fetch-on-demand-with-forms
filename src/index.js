af5b8c0dc593d0ff93daed7be3c8b3e80fe665ad
karsyn ansari <karsyn.ansari@gmail.com>
2021-12-22 23:28:29 -0700

    commit tree b2aa620aadf751dd3bfbfff3342d96df6ac6d0a9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+  //add an event listener to the form element by targeting the DOM element we want
 
-document.addEventListener('DOMContentLoaded', init);
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+    const input = document.querySelector("input#searchByID");
+    console.log(input.value);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+        inputForm.reset();
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
